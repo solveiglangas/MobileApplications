9b05657a8e02ef303e3f4a7685965d3f476e421e
karendaa <karendaa@stud.ntnu.no>
2022-03-11 14:14:04 +0100

    initial view and navigation

---
+++ USEBY/app/src/main/java/com/example/use_by/AddItem.java
@@ -0,0 +1,13 @@
+package com.example.use_by;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
--- USEBY/app/src/main/java/com/example/use_by/OpenListActivity.java
+++ USEBY/app/src/main/java/com/example/use_by/OpenListActivity.java
@@ -2,7 +2,9 @@ package com.example.use_by;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OpenListActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class OpenListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_list);
+    }
+    public void addItem(View view) {
+        Intent intent = new Intent(this, AddItem.class);
+        startActivity(intent);
     }
 }
