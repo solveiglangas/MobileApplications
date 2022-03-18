09a8ba30db24ef1da7fdd9fa2ecc9e5d00485d13
karendaa <karendaa@stud.ntnu.no>
2022-03-19 00:31:35 +0100

    lists

--- USEBY/app/src/main/java/com/example/use_by/MainActivity.java
+++ USEBY/app/src/main/java/com/example/use_by/MainActivity.java
@@ -7,9 +7,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button fridge = (Button) findViewById(R.id.button_refrigerator);
+    Button freezer = findViewById(R.id.button_freezer);
+    Button pantry = findViewById(R.id.button_pantry);
+    fridge.setOnClickListener(new OnClickListener(this));
+    freezer.setOnClickListener(new OnClickListener(this));
+    pantry.setOnClickListener(new OnClickListener(this));
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId())
+        {
+            case R.id.fridge :
+                Intent i= new Intent("com.example.OpenListActivity");
+                i.putExtra("key",value);
+                startActivity(i);
+                break;
+            case R.id.freezer :
+                Intent i= new Intent("com.example.OpenListActivity");
+                i.putExtra("key",value);
+                startActivity(i);
+                break;
+            case R.id.pantry :
+                Intent i= new Intent("com.example.OpenListActivity");
+                i.putExtra("key",value);
+                startActivity(i);
+                break;
+        }
+    }
 
 
     @Override
@@ -25,8 +55,10 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+
     public void openList(View view) {
         Intent intent = new Intent(this, OpenListActivity.class);
+        intent.putExtra("list", clickedList);
         startActivity(intent);
     }
 
