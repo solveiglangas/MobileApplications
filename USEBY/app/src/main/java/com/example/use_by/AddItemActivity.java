436bb86d98a28bc4bbf47d1271e00c9174c96324
karendaa <karendaa@stud.ntnu.no>
2022-03-21 15:43:15 +0100

    add datepicker

--- USEBY/app/src/main/java/com/example/use_by/AddItemActivity.java
+++ USEBY/app/src/main/java/com/example/use_by/AddItemActivity.java
@@ -1,22 +1,29 @@
 package com.example.use_by;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
-public class AddItemActivity extends AppCompatActivity {
+public class AddItemActivity  extends AppCompatActivity implements View.OnClickListener{
 
     private AppDatabase db;
     EditText input1;
     EditText input2;
     EditText input3;
+    EditText selectdate;
+    private int mYear, mMonth, mDay;
     String listLocation;
     int itemId;
 
@@ -25,6 +32,9 @@ public class AddItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
         db = AppDatabase.getInstance(getApplicationContext());
+
+        selectdate = findViewById ( R.id.input2 );
+        selectdate.setOnClickListener ( this );
 
         Bundle extras = getIntent().getExtras();
         System.out.println(extras);
@@ -52,6 +62,27 @@ public class AddItemActivity extends AppCompatActivity {
         intent.putExtra("list",listLocation);
         intent.putExtra("key",itemId);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        selectdate = findViewById ( R.id.input2 );
+        selectdate.setOnClickListener( this );
+        if (v == selectdate) {
+            final Calendar calendar = Calendar.getInstance ();
+            mYear = calendar.get ( Calendar.YEAR );
+            mMonth = calendar.get ( Calendar.MONTH );
+            mDay = calendar.get ( Calendar.DAY_OF_MONTH );
+
+            //show dialog
+            DatePickerDialog datePickerDialog = new DatePickerDialog ( this, new DatePickerDialog.OnDateSetListener () {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    selectdate.setText ( dayOfMonth + "/" + (month + 1) + "/" + year );
+                }
+            }, mYear, mMonth, mDay );
+            datePickerDialog.show ();
+        }
     }
 
     public void addFood(View view) {
